961e0fee7e2db13d49b4d31c7c661f6834bd21a1
Mônica Baldner <baldnermonica@gmail.com>
2025-05-26 16:55:09 -0300

    New project files

---
+++ src/main/java/com/algaworks/algasensors/ems_temperature_processing/EmsTemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.ems_temperature_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsTemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsTemperatureProcessingApplication.class, args);
+	}
+
+}
